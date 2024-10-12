643c6ea250277a5fe1d76cde26378a854396656b
MB Phong Ram <mabuphongram171@gmail.com>
2024-10-12 13:03:53 +0630

    styling with props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-background-color: blue;
-color:white;
+background-color: ${props => props.primary? 'blue':'gray'};
+color: white;
 font-size:18px;
 padding: 10px 20px;
 border-radius:5px;
 border:none;
 cursor:pointer;
 
-&:hover {
-  background-color:darkblue;
+&:hover{
+  background-color: ${props=> props.primary? 'darkblue':'darkgray'}
 }
 `
-function App() {
-return(
-  <div>
-  <Button>Click Me</Button>
-  </div>
-)
+
+function App(){
+  return(
+    <div>
+      <Button primary>
+        Primary Button
+      </Button>
+      <Button>
+        Default Button
+      </Button>
+    </div>
+  )
 }
 
 export default App
