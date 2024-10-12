b875e6e030bebc50a9df689ee39c0c8162e4d896
MB Phong Ram <mabuphongram171@gmail.com>
2024-10-12 13:32:48 +0630

    keyframes for animation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-body{
-  margin: 0;
-  font-family: Arial, sans-serif;
-  background-color: black;
-  color:white;
+
+//Styled-components supports CSS animations via the keyframes helper, allowing you to create smooth animations directly in your styled components.
+const fadeIn = keyframes`
+from {
+  opacity:0;
 }
-`
+to {
+  opacity:1;
+}
+`;
+
+const AnimateDiv = styled.div`
+animation: ${fadeIn} 3s ease-in;
+`;
 
 function App() {
   return(
     <div>
-      <GlobalStyle/>
-      <div>Welcome to Global Styling</div>
+      <AnimateDiv>
+        This is animation tutorial
+      </AnimateDiv>
     </div>
   )
 }
-
 export default App
